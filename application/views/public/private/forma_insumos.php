6f4d18fd4fe70e344e60fe3ba036049aca18b6a1
LemonKirby <101441506+LemonKirby@users.noreply.github.com>
2022-03-25 12:27:49 -0600

    Update forma_insumos.php

--- application/views/public/private/forma_insumos.php
+++ application/views/public/private/forma_insumos.php
@@ -3,77 +3,89 @@
 
 	<div class="page-header" id="banner">
 
-		<h1>Detalle insumo</h1>
+		<h1>Gestión insumos</h1>
 	</div>
 	
 
 	<div class="pt-2">
 		<div class="row m-2">
-			<nav aria-label="breadcrumb">
-				<ol class="breadcrumb">
-					<li class="breadcrumb-item active" aria-current="page"><i class="fa-solid fa-angle-left"></i> Volver atras</li>
-				</ol>
-			</nav>
 		</div>
-
 		<div class="row m-2">
-			<div class="card p-2">
-				<div class="row">
-					<div class="col-12">
-					    <div class="form-group">
-					      <label for="exampleInputEmail1" class="form-label mt-4">Nombre</label>
-					      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nombre del insumo">
-					      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-					    </div>
-
-					    <div class="form-group">
-					      <label for="exampleSelect2" class="form-label mt-4">Unidad de medida</label>
-					      <select class="form-select" id="exampleSelect2">
-					        <option value="lts">Litro (lts.)</option>
-					        <option value="ml">Mililitro (ml.)</option>
-					        <option value="cc">Centímetros cúbicos (c.c.)</option>
-					        <option value="kg">Kilogramos(kg.)</option>
-					        <option value="gr">Gramos (gr.)</option>
-					        <option value="lb">Libra (lb.)</option>
-					        <option value="oz">Onza (oz.)</option>
-					      </select>
-					    </div>
-
-					    <div class="form-group">
-					      <label for="exampleSelect2" class="form-label mt-4">Proveedor</label>
-					      <select class="form-select" id="exampleSelect2">
-					        <option value="1">Proveedor 1</option>
-					        <option value="2">Proveedor 2</option>
-					      </select>
-					    </div>
-
+			<div class="row">
+				<div class="col-4">
+					<div class="input-group">
+						<input type="text" class="form-control form-control-sm">
+						<div class="input-group-prepend">
+							<button class="btn btn-outline-primary btn-sm" type="button"><i class="fa-solid fa-magnifying-glass"></i></button>
+						</div>
+						
 					</div>
-					<div class="row m-4"></div>
-
-
-					<div class="col">
-					</div>
-					<div class="col">
-						<button class="form-control btn btn-outline-primary btn-sm" type="button">GUARDAR <i class="fa-solid fa-chart-simple"></i></button>
-						<div class="row mt-2"></div>
-						<!-- <button class="form-control btn btn-outline-primary btn-sm" type="button">EXPORTAR A EXCEL <i class="fa-solid fa-file-export"></i></button> -->
-					</div>
+				</div>
+				<div class="col">
+					<!-- <button class="form-control btn btn-primary btn-sm" type="button">CREAR INSUMO <i class="fa-solid fa-plus"></i></button> -->
+				</div>
+				<div class="col">
+					<button class="form-control btn btn-primary btn-sm" type="button">HISTORIAL <i class="fa-solid fa-chart-simple"></i></button>
+				</div>
+				<div class="col">
+					<a href="<?=base_url('/controlador/insumosAdd')?>" class="form-control btn btn-primary btn-sm" type="button">AGREGAR <i class="fa-solid fa-chart-simple"></i></a>
+					<div class="row mt-2"></div>
+					<!-- <button class="form-control btn btn-outline-primary btn-sm" type="button">EXPORTAR A EXCEL <i class="fa-solid fa-file-export"></i></button> -->
 				</div>
 			</div>
 		</div>
 
+
+		<div class="row m-2">
+			<div class="table-responsive">			
+ 		<!-- table -->
+				<table class="table table-hover table-sm table-bordered">
+				  <thead class="table-dark">
+				    <tr>
+				      <th scope="col">#</th>
+				      <th scope="col">INSUMO</th>
+				      <th scope="col">PROVEEDOR</th>
+				      <th scope="col">MARCA</th>
+				      <th scope="col">PRESENTACION</th>
+				      <th scope="col">DETALLES</th>
+				      <th scope="col">STOCK</th>
+				      <th scope="col">ACCIONES</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				  	<?php 
+
+				  	$datos = array('Azucar','Harina de trigo','Harina de maiz', 'Aceite', 'Pollo', 'Chocolate');
+				  	$marcas = array('Mars','Kraft','General Mills');
+
+				  	for ($i=0; $i <6 ; $i++) { ?>
+
+				    <tr>
+				      <th scope="row"><?php echo($i+1) ?></th>
+				      <td><?php echo($datos[$i]) ?></td>
+				      <td><?php echo('Proveedor '.random_int(1, 99)) ?></td>
+				      <td><?php $temp = array_rand($marcas); echo($marcas[$temp]) ?></td>
+				      <td><?php echo(random_int(100, 699).' grs') ?></td>
+				      <td><a href="<?=base_url('/controlador/detalleinsumo')?>">Ver mas </a></td>
+				      <td><?php echo(random_int(20, 45).' u') ?></td>
+				      <td>
+				      	
+						<a href="<?=base_url('/controlador/detalleinsumo')?>" type="button" class="btn btn-outline-dark btn-sm"><i class="fa-solid fa-pen-to-square"></i></a>
+						<a href="<?=base_url('/controlador/detalleinsumo')?>" type="button" class="btn btn-outline-dark btn-sm"><i class="fa-solid fa-trash"></i></a>
+						<button type="button" class="btn btn-outline-dark btn-sm">Merma</button>
+						<button type="button" class="btn btn-outline-dark btn-sm">Historial Merma</button>
+				      </td>
+				    </tr> 
+				  	<?php
+				  	}
+
+				  	?>
+
+				  </tbody>
+				</table>
+ 		<!-- ./table -->
+ 			</div>
+		</div>
+
+
 	</div>
-
-	<div class="row m-3"></div>
-
-<!-- 	<div class="d-flex justify-content-around m-2">
-		<a href="http://localhost/inventario/">Login</a>
-		<a href="http://localhost/inventario/controlador/puntosVenta">Puntos de venta</a>
-		<a href="http://localhost/inventario/controlador/insumos">Insumos</a>
-		<a href="http://localhost/inventario/controlador/recetas">Recetas</a>
-		<a href="http://localhost/inventario/controlador/productos">productos</a>
-		<a href="http://localhost/inventario/controlador/reportes">Reportes</a>
-	</div> -->
-	
-
-</div>
