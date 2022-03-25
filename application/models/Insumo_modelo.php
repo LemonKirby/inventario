068a958fb3b15f5de60cdf3c0c3b3287a6b567d9
LemonKirby <101441506+LemonKirby@users.noreply.github.com>
2022-03-25 12:37:00 -0600

    InsumoAdd

--- application/models/Insumo_modelo.php
+++ application/models/Insumo_modelo.php
@@ -4,45 +4,35 @@ defined('BASEPATH') OR exit('No se permite el acceso directo.');
 class Insumo_model extends CI_Model{
 
 	public function getAll(){
-		$this->db->join('insumo', 'proveedor.idProveedor = insumo.idProveedor');
-		$rs = $this->db->get('insumo');
-		return $rs->num_rows() > 0 ? $rs->result() : NULL;
+        $this->db->select("*");
+        $this->db->from("insumo");
+        $results=$this->db->get();
+        return $results->result();
 	}
 
-	public function insert($data){
+	public function save($data){
 		return $this->db->insert('insumo', $data) ? true : NULL;
 	}
 
 	public function getById($id)
     {
-        $cmd = "SELECT * FROM insumo where idInsumo  = ".$id;
-
-        $query = $this->db->query($cmd);
-        return ($query->num_rows() == 1) ? $query->row() : NULL;
+        $this->db->select("*");
+        $this->db->from("insumo");
+        $this->db->where("idInsumo",$id);
+        $result=$this->db->get();
+        return $result->row();   
     }
 
-	public function update($entry, $idInsumo)
-    {
-        try {
-            $this->db->set($entry);
-            $this->db->where('idInsumo', $idInsumo);
-            $this->db->update('v');
-
-            return ($idInsumo) ? TRUE : NULL;
-        }
-
-        catch(Exception $e) {
-            return $e->getMessage();
-        }
-		
+	public function update($data, $id)
+    { 
+        $this->db->where("idInsumo",$id);
+        $this->db->update("insumo",$data);
     }	
 
-	public function deleteById($id)
+	public function delete($id)
     {
-        $cmd = "DELETE FROM insumo where idInsumo = ".$id;
-
-        $query = $this->db->query($cmd);
-        return (TRUE) ? TRUE : NULL;
+        $this->db->where("idInsumo",$id);
+        $this->db->delete("insumo");
     }
      
 }
